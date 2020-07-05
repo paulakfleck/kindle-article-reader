5fc83e67fdee24a2630889252b4862cdea3edf7e
Paula Fleck <paulakfleck@gmail.com>
2020-07-05 17:38:25 -0300

    Mercury script to Mecury AMP

---
+++ assets/script.js
@@ -0,0 +1,95 @@
+// kindle does not support const
+var MERCURY = 'https://mercury.postlight.com/amp?url=',
+    CORS = 'https://cors-anywhere.herokuapp.com/',
+    currUrl = location.href,
+    articlesList = '<ul>',
+    rssLink,
+    RSS_URL;
+
+function getAndParseRss() {
+    if (currUrl.indexOf('?rssLink=') > -1) {
+
+        // Get link from URL
+        rssLink = currUrl.split('?rssLink=');
+        rssLink = decodeURIComponent(rssLink[1]);
+        
+        RSS_URL = CORS + rssLink;
+
+        $.ajax(RSS_URL, {
+            accepts: {
+                xml: 'application/rss+xml'
+            },
+            dataType: 'xml',
+            success: function (data) {
+                try {
+                    document.getElementById('rss-input').style.display = 'none';
+
+                    $(data).find('item').each(function () {
+                        var el = $(this);
+    
+                        var title = el.find('title').text();
+                        var link = el.find('link').text();
+    
+                        articlesList += '<li>' + title + ' | <a class="article-mode" href="' + link + '">Link<a></li>';
+                    });
+                    articlesList += '</ul>';
+    
+                    document.querySelector('main').insertAdjacentHTML('beforeend', articlesList);
+    
+                } catch (error) {
+                    // Detect errors on Kindle, since there's no console there
+                    alert(error);
+                }
+            },
+            error: function (err) {
+                // Detect errors on Kindle, since there's no console there
+                alert('failed');
+            }
+        });
+    }
+
+}
+
+function isDev() {
+    if (currUrl.indexOf('isDev') > -1) {
+        document.write(navigator.userAgent);
+        return true;
+    }
+}
+
+$(document).ready(function () {
+
+    if (!isDev()) {
+        getAndParseRss();
+    }
+
+    $(document).on('click', '.article-mode', function (e) {
+        e.preventDefault();
+        // try {
+        var url = CORS + MERCURY + $(this).attr('href');
+
+        e.preventDefault();
+        try {
+    
+            $.ajax({
+                url: url,
+                type: 'GET',
+                success: function(res) {
+
+                    res = res.split('<main class="hg-article-container" role="main">');
+                    res = res[1];
+
+                    
+                    if (res.indexOf('amp-img') > -1) {
+                        res = res.replace(/amp-img/g, 'img');
+                    }
+                    
+                    document.querySelector('body').classList.add('modal-opened');
+                    document.getElementById('modal').insertAdjacentHTML('beforeend', res);
+                }
+            });
+        } catch (error) {
+            alert(error);
+        }
+    });
+});
